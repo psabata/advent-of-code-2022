92283d6236cf99fd7d354cd02bc38db37699ed71
Petr Šabata <petr.sabata@kiwi.com>
2022-12-03 17:02:39 +0100

    Day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,22 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+
+    fun part1(input: List<List<Int>>): Int {
+
+        return input.maxOf { it.sum() }
+
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<List<Int>>): Int {
+
+        return input.sortedBy { it.sum() }.takeLast(3).sumOf { it.sum() }
+
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = InputHelper("Day01_test.txt").readIntGroups()
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    val input = InputHelper("Day01.txt").readIntGroups()
+    println("Part 1: ${part1(input)}")
+    println("Part 2: ${part2(input)}")
 }
