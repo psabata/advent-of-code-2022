06d80cd6ea5a940509b5b8a29c9843c359c30d4d
Petr Šabata <petr.sabata@kiwi.com>
2022-12-09 11:28:04 +0100

    Day 08 - enclose code in object to avoid clashes between days

--- src/Day08.kt
+++ src/Day08.kt
@@ -2,111 +2,118 @@ import kotlin.math.max
 
 fun main() {
 
-    fun part1(input: List<List<Int>>): Int {
+    val testInput = Day08.Forrest(InputHelper("Day08_test.txt").readLines())
+    check(testInput.part1() == 21)
+    check(testInput.part2() == 8)
 
-        var visible = 0
+    val input = Day08.Forrest(InputHelper("Day08.txt").readLines())
+    println("Part 1: ${input.part1()}")
+    println("Part 2: ${input.part2()}")
 
-        input.forEachIndexed { y, row ->
-            row.forEachIndexed { x, _ ->
-                if (input.isVisible(x, y)) visible++
-            }
-        }
-
-        return visible
-
-    }
-
-    fun part2(input: List<List<Int>>): Int {
-
-        var scenicScore = 0
-
-        input.forEachIndexed { y, row ->
-            row.forEachIndexed { x, _ ->
-                scenicScore = max(input.scenicScore(x, y), scenicScore)
-            }
-        }
-
-        return scenicScore
-
-    }
-
-    val testInput = InputHelper("Day08_test.txt").readLines { line -> line.map { it.digitToInt() } }
-    check(part1(testInput) == 21)
-    check(part2(testInput) == 8)
-
-    val input = InputHelper("Day08.txt").readLines { line -> line.map { it.digitToInt() } }
-    println("Part 1: ${part1(input)}")
-    println("Part 2: ${part2(input)}")
 }
 
-fun List<List<Int>>.scenicScore(x: Int, y: Int): Int {
+object Day08 {
 
-    val scenicScore = scenicScore(x, y, Direction.UP) *
-            scenicScore(x, y, Direction.DOWN) *
-            scenicScore(x, y, Direction.LEFT) *
-            scenicScore(x, y, Direction.RIGHT)
+    class Forrest(input: List<String>) {
+
+        private val trees: List<List<Int>> = input.map { line -> line.map { it.digitToInt() } }
+
+        fun part1(): Int {
+            var visibleTrees = 0
+
+            trees.forEachIndexed { y, row ->
+                row.forEachIndexed { x, _ ->
+                    if (trees.isVisible(x, y)) visibleTrees++
+                }
+            }
+
+            return visibleTrees
+        }
+        fun part2(): Int {
+
+            var scenicScore = 0
+
+            trees.forEachIndexed { y, row ->
+                row.forEachIndexed { x, _ ->
+                    scenicScore = max(trees.scenicScore(x, y), scenicScore)
+                }
+            }
+
+            return scenicScore
+
+        }
+
+    }
+
+    private fun List<List<Int>>.scenicScore(x: Int, y: Int): Int {
+
+        val scenicScore = scenicScore(x, y, Direction.UP) *
+                scenicScore(x, y, Direction.DOWN) *
+                scenicScore(x, y, Direction.LEFT) *
+                scenicScore(x, y, Direction.RIGHT)
 
 //    println("x: $x, y: $y, tree: ${this[y][x]}, scenicScore: $scenicScore")
 
-    return scenicScore
-}
+        return scenicScore
+    }
 
-fun List<List<Int>>.isVisible(x: Int, y: Int): Boolean {
-    val isVisible = isVisible(x, y, Direction.UP)
-            || isVisible(x, y, Direction.DOWN)
-            || isVisible(x, y, Direction.LEFT)
-            || isVisible(x, y, Direction.RIGHT)
+    private fun List<List<Int>>.isVisible(x: Int, y: Int): Boolean {
+        val isVisible = isVisible(x, y, Direction.UP)
+                || isVisible(x, y, Direction.DOWN)
+                || isVisible(x, y, Direction.LEFT)
+                || isVisible(x, y, Direction.RIGHT)
 
 //    println("x: $x, y: $y, tree: ${this[y][x]}, visible: $isVisible")
 
-    return isVisible
-}
+        return isVisible
+    }
 
-private fun List<List<Int>>.isVisible(
-    x: Int,
-    y: Int,
-    direction: Direction,
-): Boolean {
-    var newX = x
-    var newY = y
+    private fun List<List<Int>>.isVisible(
+        x: Int,
+        y: Int,
+        direction: Direction,
+    ): Boolean {
+        var newX = x
+        var newY = y
 
-    do {
-        newX += direction.dx
-        newY += direction.dy
+        do {
+            newX += direction.dx
+            newY += direction.dy
 
-        if (newY in indices && newX in this[newY].indices) {
-            if (this[newY][newX] >= this[y][x]) return false
-        } else {
-            return true
-        }
-    } while (true)
-}
+            if (newY in indices && newX in this[newY].indices) {
+                if (this[newY][newX] >= this[y][x]) return false
+            } else {
+                return true
+            }
+        } while (true)
+    }
 
-private fun List<List<Int>>.scenicScore(
-    x: Int,
-    y: Int,
-    direction: Direction,
-): Int {
-    var newX = x
-    var newY = y
-    var scenicScore = 0
+    private fun List<List<Int>>.scenicScore(
+        x: Int,
+        y: Int,
+        direction: Direction,
+    ): Int {
+        var newX = x
+        var newY = y
+        var scenicScore = 0
 
-    do {
-        newX += direction.dx
-        newY += direction.dy
+        do {
+            newX += direction.dx
+            newY += direction.dy
 
-        if (newY in indices && newX in this[newY].indices) {
-            scenicScore++
-            if (this[newY][newX] >= this[y][x]) return scenicScore
-        } else {
-            return scenicScore
-        }
-    } while (true)
-}
+            if (newY in indices && newX in this[newY].indices) {
+                scenicScore++
+                if (this[newY][newX] >= this[y][x]) return scenicScore
+            } else {
+                return scenicScore
+            }
+        } while (true)
+    }
+    private enum class Direction(val dx: Int, val dy: Int) {
+        UP(0, -1),
+        DOWN(0, 1),
+        LEFT(-1, 0),
+        RIGHT(1, 0)
+    }
 
-enum class Direction(val dx: Int, val dy: Int) {
-    UP(0, -1),
-    DOWN(0, 1),
-    LEFT(-1, 0),
-    RIGHT(1, 0)
 }
